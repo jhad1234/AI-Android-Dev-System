88493f3651bfcea9f8fed4f041cef26ba7fa0381
jhad1234 <pxx7168311@gmail.com>
2026-05-18 01:40:51 +0300

    ok

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AI-Android-Dev-System"
+include(":app")
