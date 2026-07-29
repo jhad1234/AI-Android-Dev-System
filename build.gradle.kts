65301393a7f6121d371271dfe3b6fd1ad67e4ce2
jhad1234 <pxx7168311@gmail.com>
2026-07-29 23:44:43 +0000

    Fix build.gradle.kts and restore missing wrapper jar for CI/CD [Manus AI]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
